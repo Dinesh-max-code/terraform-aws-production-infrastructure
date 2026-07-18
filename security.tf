1daff62faf087bd9dad00c1e2a06917fd1ce4c93
Dinesh-max-code <dinesh.rit.18@gmail.com>
2026-07-18 12:44:21 +0530

    Add production security groups

---
+++ security.tf
@@ -0,0 +1,61 @@
+resource "aws_security_group" "alb" {
+  name        = "prod-alb-sg"
+  description = "Security Group for Application Load Balancer"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "prod-alb-sg"
+  }
+}
+
+
+
+resource "aws_security_group" "ec2" {
+  name        = "prod-ec2-sg"
+  description = "Security Group for EC2 Instances"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "Allow HTTP from ALB only"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "prod-ec2-sg"
+  }
+}
